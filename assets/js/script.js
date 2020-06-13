0c612d78f463c6de27648414245f46a351a87f55
geocode-matt <mlindenx@gmail.com>
2020-06-13 11:45:25 -0500

    quiz starting page initial formatting built

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
-console.log("hi");
+var node = document.createElement("li");
+var textNode = document.createTextNode("Water");
+node.appendChild(textNode);
+document.getElementById("myList").appendChild(node);
